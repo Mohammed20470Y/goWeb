2ef7804c3d40febe6b015eac7164545da34af99a
Mohammed20470Y <Mohammadyounisy@gmail.com>
2025-01-23 22:44:24 +0300

    Making the server run staticlyy

--- main/main.go
+++ main/main.go
@@ -5,21 +5,24 @@ import (
 	"net/http"
 )
 
-func main() {
+var (
+	mux http.ServeMux
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	// Serve static files (CSS, JS, images)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	tmpl, err := template.ParseFiles("cosy.html")
+	if err != nil {
+		http.Error(w, "could not load template", http.StatusInternalServerError)
+		return
+	}
+	data := map[string]string{"Title": "store"}
+	tmpl.Execute(w, data)
+}
 
-	// Define handler for rendering HTML template
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("cosy.html")
-		if err != nil {
-			http.Error(w, "could not load template", http.StatusInternalServerError)
-			return
-		}
-		data := map[string]string{"Title": "CosyPOS"}
-		tmpl.Execute(w, data)
-	})
-
-	// Start the server
-	http.ListenAndServe(":8080", nil)
+func main() {
+	println("server is run localhost and on port 8080")
+	mux.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", &mux)
 }
