a9f09de3e575fc5fc14b95a6899992baf8b7faf0
Mohammed20470Y <Mohammadyounisy@gmail.com>
2025-01-23 22:03:59 +0300

    cosy is working

--- main/main.go
+++ main/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>hello Mohammed Ismail</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	// Serve static files (CSS, JS, images)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Define handler for rendering HTML template
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("cosy.html")
+		if err != nil {
+			http.Error(w, "could not load template", http.StatusInternalServerError)
+			return
+		}
+		data := map[string]string{"Title": "CosyPOS"}
+		tmpl.Execute(w, data)
+	})
+
+	// Start the server
 	http.ListenAndServe(":8080", nil)
 }
